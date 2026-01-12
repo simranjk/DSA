c2551935e7c8804c1b4d61fa9d24bcf29d53f84c
simranjeetkaur <simranjk.rana@gmail.com>
2026-01-11 22:18:25 -0500

    finding two numbers in an array that add up to a given target number

---
+++ Array/TwoSum.java
@@ -0,0 +1,29 @@
+public class TwoSum {
+
+  static boolean twoSum(int[] arr, int target){
+    int n = arr.length;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+
+        if (arr[i] + arr[j] == target) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  public static void main(String[] args){
+
+    int[] arr = { 0, -1, 2, -3, 1 };
+    int target = -2;
+
+    if (twoSum(arr, target))
+      System.out.println("true");
+    else
+      System.out.println("false");
+  }
+
+}
