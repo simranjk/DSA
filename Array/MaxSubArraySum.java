aea10174e36caf5ccdbcf51a2dd972e6303a61ed
simranjeetkaur <simranjk.rana@gmail.com>
2026-01-21 18:21:22 -0500

    finding max subarray sum with brute force as well

--- Array/MaxSubArraySum.java
+++ Array/MaxSubArraySum.java
@@ -1,4 +1,35 @@
 
+//Naive approach ------------------------->
+
+public class MaxSubArraySum{
+  private static int maxSum(int[] arr, int n) {
+    int res= arr[0];
+
+    for(int i=0; i<n; i++){
+      int curr=0;
+      for(int j=i; j<n; j++){
+        curr=curr + arr[j];
+        res = Math.max(res, curr);
+      }
+    }
+    return res;
+  }
+
+
+  public static void main(String[] args) {
+    int[] arr = {-3, 8, -2, 4, -5, 6};
+    int n = 6;
+
+    int res = maxSum(arr, n);
+    System.out.println(res);
+  }
+
+}
+
+
+
+
+
 //Efficient approach ------------------------->
 
 public class MaxSubArraySum {
