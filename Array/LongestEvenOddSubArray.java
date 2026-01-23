3c1dd443a09dee08a4f28d8622cdc3adb2c555bb
simranjeetkaur <simranjk.rana@gmail.com>
2026-01-22 19:43:18 -0500

    finding longest even odd subarray with brute force

---
+++ Array/LongestEvenOddSubArray.java
@@ -0,0 +1,33 @@
+//Naive approach -------------------------->
+
+public class LongestEvenOddSubArray {
+  static int maxEvenOdd(int arr[], int n)
+  {
+    int res = 1;
+
+    for(int i = 0; i < n; i++)
+    {
+      int curr = 1;
+
+      for(int j = i + 1; j < n; j++)
+      {
+        if((arr[j] % 2 == 0 && arr[j - 1] % 2 != 0)
+          ||(arr[j] % 2 != 0 && arr[j - 1] % 2 == 0))
+          curr++;
+        else
+          break;
+      }
+
+      res = Math.max(res, curr);
+    }
+
+    return res;
+  }
+
+  public static void main(String args[]){
+    int arr[] = {1, 2, 3, 4, 5, 7, 9} , n = 5;
+    System.out.print(maxEvenOdd(arr, n));
+
+  }
+
+}
