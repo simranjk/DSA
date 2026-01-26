55ae5527528ad78d51945b797f70365826f28d72
simranjeetkaur <simranjk.rana@gmail.com>
2026-01-26 16:13:19 -0500

    Finding the minimum consecutive flips in an array in o(n)

---
+++ Array/MinimumConsecutiveFlips.java
@@ -0,0 +1,33 @@
+public class MinimumConsecutiveFlips {
+
+
+  static void printGroups(int arr[], int n)
+  {
+
+    for(int i = 1; i < n; i++)
+    {
+
+      if (arr[i] != arr[i - 1])
+      {
+        if (arr[i] != arr[0])
+          System.out.print("From " + i + " to ");
+        else
+          System.out.println(i - 1);
+      }
+    }
+
+    if (arr[n - 1] != arr[0])
+      System.out.println(n - 1);
+  }
+
+  // Driver code
+  public static void main(String[] args)
+  {
+    int arr[] = {0, 1, 1, 0, 0, 0, 1, 1};
+    int n = arr.length;
+
+    printGroups(arr, n);
+  }
+}
+
+
