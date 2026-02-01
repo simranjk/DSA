f6e6ac3668a266354898cb7d852375a94c2b8d40
simranjeetkaur <simranjk.rana@gmail.com>
2026-01-31 19:54:44 -0500

    solution with naive approach as well

--- Array/MaxAppearingElement.java
+++ Array/MaxAppearingElement.java
@@ -1,3 +1,5 @@
+//Efficient approach -------------------->
+
 public class MaxAppearingElement {
   static int MAX = 1000000;
   public static int maximumOccurredElement(int[] L, int[] R, int n){
@@ -33,3 +35,38 @@ public class MaxAppearingElement {
   }
   }
 
+
+
+//Naive approach -------------------->
+
+public class MaxAppearingElement {
+
+  public static void main(String[] args){
+    int[] l = { 1, 4, 9, 13, 21 };
+    int[] r = { 15, 8, 12, 20, 30 };
+    int n = l.length;
+    System.out.println(maximumOccurredElement(l, r, n));
+  }
+
+  private static int maximumOccurredElement(int[] l, int[] r, int n) {
+
+    int[] freq = new int[101];
+
+    for(int i=0; i<n ; i++){
+      freq[l[i]]++;
+      freq[r[i]+1]--;
+    }
+
+    int res=0;
+    for(int i=1; i<100 ; i++){
+      freq[i]=freq[i-1] +freq[i];
+      if(freq[i]>freq[res]){
+        res=i;
+      }
+    }
+
+
+
+    return res;
+  }
+}
