1b998a56bd9823486568f872f2a150dd5ca4ec79
simranjeetkaur <simranjk.rana@gmail.com>
2026-02-07 16:13:32 -0500

    finding leftmost repeating character in the string in o(n square)

---
+++ String/LeftmostRepeatingChar.java
@@ -0,0 +1,20 @@
+//Naive approach ------------------->
+
+
+public class LeftmostRepeatingChar {
+  static int isRepeating(String str){
+    for(int i=0; i<str.length(); i++){
+      for(int j=i+1; j<str.length(); j++){
+        if(str.charAt(i)==str.charAt(j)){
+          return i;
+        }
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    String str = "abcbc";
+    System.out.print(isRepeating(str));
+  }
+}
