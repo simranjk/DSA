f23294a69b0189d5c398ed6cadee81acee2ec952
simranjeetkaur <simranjk.rana@gmail.com>
2026-02-08 08:22:35 -0500

    finding leftmost repeating character in o(logn)

--- String/LeftmostRepeatingChar.java
+++ String/LeftmostRepeatingChar.java
@@ -1,6 +1,5 @@
 //Naive approach ------------------->
 
-
 public class LeftmostRepeatingChar {
   static int isRepeating(String str){
     for(int i=0; i<str.length(); i++){
@@ -12,6 +11,34 @@ public class LeftmostRepeatingChar {
     }
     return -1;
   }
+
+  public static void main(String[] args) {
+    String str = "abcbc";
+    System.out.print(isRepeating(str));
+  }
+}
+
+
+
+//Better approach -------------------->
+
+public class LeftmostRepeatingChar {
+  static int isRepeating(String str){
+
+    int CHAR =256;
+    int count[]=new int[CHAR];
+
+    for(int i=0; i<str.length(); i++){
+      count[str.charAt(i)]++;
+    }
+    for(int i=0; i<str.length(); i++){
+      if (count[str.charAt(i)]>1){
+        return i;
+      }
+    }
+    return -1;
+  }
+
 
   public static void main(String[] args) {
     String str = "abcbc";
