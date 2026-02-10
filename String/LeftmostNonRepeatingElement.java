058a741bfd93c36377279ea754e7ca7f3ff13582
simranjeetkaur <simranjk.rana@gmail.com>
2026-02-09 21:10:50 -0500

    finding left most repeating with efiicient way

--- String/LeftmostNonRepeatingElement.java
+++ String/LeftmostNonRepeatingElement.java
@@ -27,3 +27,33 @@ public class LeftmostNonRepeatingElement {
   }
 
 }
+
+
+
+
+//Efficient approach ------------------>
+
+public class LeftmostNonRepeatingElement {
+  public static void main(String[] args) {
+    String str = "abcbc";
+    System.out.print(isNotRepeating(str));
+  }
+
+  static int isNotRepeating(String str) {
+
+    int CHAR = 256;
+    int[] f1 = new int[CHAR];
+
+    for (int i = 0; i < str.length(); i++) {
+      f1[str.charAt(i)]++;
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+      if (f1[str.charAt(i)] == 1) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
