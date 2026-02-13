3b475948e416d25ce7fe00fd313e1b68504a9bc5
simranjeetkaur <simranjk.rana@gmail.com>
2026-02-13 18:02:26 -0500

    Another approach simple but with extra space

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -35,3 +35,28 @@ public class ReverseString {
 
 }
 
+
+
+// Using String.split() ----------------------->
+
+
+public class ReverseString {
+
+    public static String reverseWords(String s) {
+      String[] words = s.split(" ");
+      StringBuilder result = new StringBuilder();
+
+      for (int i = words.length - 1; i >= 0; i--) {
+        result.append(words[i]);
+        if (i != 0) result.append(" ");
+      }
+
+      return result.toString();
+    }
+
+    public static void main(String[] args) {
+      String s = "My name is Simran";
+      System.out.println(reverseWords(s));
+    }
+
+}
