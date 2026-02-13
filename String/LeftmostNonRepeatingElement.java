fcf40aee72fd8f81c6c6545ffe093d4084b79d29
simranjeetkaur <simranjk.rana@gmail.com>
2026-02-12 22:39:10 -0500

    one more approach for this solution

--- String/LeftmostNonRepeatingElement.java
+++ String/LeftmostNonRepeatingElement.java
@@ -57,3 +57,30 @@ public class LeftmostNonRepeatingElement {
     return -1;
   }
 }
+
+
+
+
+//Better approach ------------------->
+
+  public class LeftmostNonRepeatingElement
+ {
+  static final int CHAR=256;
+  static int nonRep(String str)
+  {
+    int[] count=new int[CHAR];
+    for(int i=0;i<str.length();i++){
+      count[str.charAt(i)]++;
+    }
+    for(int i=0;i<str.length();i++){
+      if(count[str.charAt(i)]==1)return i;
+    }
+    return -1;
+  }
+
+  public static void main(String args[])
+  {   String str = "geeky";
+    System.out.println("Index of leftmost non-repeating element:");
+    System.out.println(nonRep(str));
+  }
+}
